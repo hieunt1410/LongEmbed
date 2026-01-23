18e2ccc49eb8b888296e91558f5cec681cffdd54
hieungo1410 <hieungo1410@gmail.com>
2026-01-23 16:48:23 +0700

    new import

--- src/LEMBPasskeyRetrieval.py
+++ src/LEMBPasskeyRetrieval.py
@@ -1,13 +1,8 @@
 import datasets
-
-from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
-
-
+from mteb.abstasks.task_metadata import TaskMetadata
+from mteb.abstasks.retrieval import AbsTaskRetrieval
 class LEMBPasskeyRetrieval(AbsTaskRetrieval):
     _EVAL_SPLIT = "test"
-
     metadata = TaskMetadata(
         name="LEMBPasskeyRetrieval",
         dataset={
@@ -35,21 +30,17 @@ class LEMBPasskeyRetrieval(AbsTaskRetrieval):
         n_samples={_EVAL_SPLIT: 1200},
         avg_character_length={_EVAL_SPLIT: 28994.8},
     )
-
     def load_data(self, **kwargs):
         if self.data_loaded:
             return
-
         if "context_length" not in kwargs:
             raise ValueError("Need to specify context_length")
         context_length = kwargs["context_length"]
-
         query_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "queries"
         ]  # dict_keys(['qid', 'text'])
         query_list = query_list.filter(lambda x: x["context_length"] == context_length)
         queries = {row["qid"]: row["text"] for row in query_list}
-
         corpus_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "corpus"
         ]  # dict_keys(['doc_id', 'text'])
@@ -57,15 +48,12 @@ class LEMBPasskeyRetrieval(AbsTaskRetrieval):
             lambda x: x["context_length"] == context_length
         )
         corpus = {row["doc_id"]: {"text": row["text"]} for row in corpus_list}
-
         qrels_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "qrels"
         ]  # dict_keys(['qid', 'doc_id'])
         qrels_list = qrels_list.filter(lambda x: x["context_length"] == context_length)
         qrels = {row["qid"]: {row["doc_id"]: 1} for row in qrels_list}
-
         self.corpus = {self._EVAL_SPLIT: corpus}
         self.queries = {self._EVAL_SPLIT: queries}
         self.relevant_docs = {self._EVAL_SPLIT: qrels}
-
         self.data_loaded = True
--- src/LEMBWikimQARetrieval.py
+++ src/LEMBWikimQARetrieval.py
@@ -1,13 +1,8 @@
 import datasets
-
-from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
-
-
+from mteb.abstasks.task_metadata import TaskMetadata
+from mteb.abstasks.retrieval import AbsTaskRetrieval
 class LEMBWikimQARetrieval(AbsTaskRetrieval):
     _EVAL_SPLIT = "test"
-
     metadata = TaskMetadata(
         name="LEMBWikimQARetrieval",
         dataset={
@@ -43,30 +38,23 @@ class LEMBWikimQARetrieval(AbsTaskRetrieval):
         n_samples={_EVAL_SPLIT: 500},
         avg_character_length={_EVAL_SPLIT: 37513},
     )
-
     def load_data(self, **kwargs):
         if self.data_loaded:
             return
-
         query_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "queries"
         ]  # dict_keys(['qid', 'text'])
         queries = {row["qid"]: row["text"] for row in query_list}
-
         corpus_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "corpus"
         ]  # dict_keys(['doc_id', 'text'])
         corpus = {row["doc_id"]: {"text": row["text"]} for row in corpus_list}
-
         qrels_list = datasets.load_dataset(**self.metadata_dict["dataset"])[
             "qrels"
         ]  # dict_keys(['qid', 'doc_id'])
         qrels = {row["qid"]: {row["doc_id"]: 1} for row in qrels_list}
-
         self.corpus = {self._EVAL_SPLIT: corpus}
         self.queries = {self._EVAL_SPLIT: queries}
         self.relevant_docs = {self._EVAL_SPLIT: qrels}
-
         self.data_loaded = True
-
         self.calculate_metadata_metrics()
