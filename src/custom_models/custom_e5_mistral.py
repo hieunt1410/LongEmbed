4e219f877afac0f2f3164c1b3a07d11dbb5c14c4
hieungo1410 <hieungo1410@gmail.com>
2026-01-30 18:22:41 +0700

    fix: adjust `tp_exiting_index` in custom E5-Mistral model from 16 to 6.

--- src/custom_models/custom_e5_mistral.py
+++ src/custom_models/custom_e5_mistral.py
@@ -241,7 +241,7 @@ class MistralModel(MistralPreTrainedModel):
         self.post_init()
         self.plan = "tp"
         self.tp_starting_index = 1
-        self.tp_exiting_index = 16
+        self.tp_exiting_index = 6
 
     def get_input_embeddings(self):
         return self.embed_tokens
