c6a37742f659e8419d658eb1bb95b4808583a608
hieungo1410 <hieungo1410@gmail.com>
2026-01-30 15:51:38 +0700

    @@

--- src/custom_models/custom_e5_mistral.py
+++ src/custom_models/custom_e5_mistral.py
@@ -619,6 +619,14 @@ class MistralForCausalLM(MistralPreTrainedModel):
         **kwargs: Unpack[TransformersKwargs],
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         r"""
+        Args:
+            labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ..., config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        Returns:
+
         Example:
 
         ```python
