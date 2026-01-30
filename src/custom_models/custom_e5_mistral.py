0ee79e0789d3401d89a2319fe43a55b925a5e983
hieungo1410 <hieungo1410@gmail.com>
2026-01-30 15:19:52 +0700

    MistralAttention

--- src/custom_models/custom_e5_mistral.py
+++ src/custom_models/custom_e5_mistral.py
@@ -17,7 +17,6 @@ from transformers.models.mistral.modeling_mistral import (
     MistralMLP,
     MistralRMSNorm,
     MistralPreTrainedModel,
-    MistralAttention
 )
 from transformers.cache_utils import Cache, DynamicCache, StaticCache
 from transformers.modeling_attn_mask_utils import AttentionMaskConverter
@@ -112,172 +111,172 @@ def scaled_dot_product_attention(
     return attn_weight @ value
 
 
-# class MistralAttention(nn.Module):
-#     """Multi-headed attention from 'Attention Is All You Need' paper"""
+class MistralAttention(nn.Module):
+    """Multi-headed attention from 'Attention Is All You Need' paper"""
 
-#     def __init__(self, config: MistralConfig, layer_idx: Optional[int] = None):
-#         super().__init__()
-#         self.config = config
-#         self.layer_idx = layer_idx
-#         if layer_idx is None:
-#             logger.warning_once(
-#                 f"Instantiating {self.__class__.__name__} without passing a `layer_idx` is not recommended and will "
-#                 "lead to errors during the forward call if caching is used. Please make sure to provide a `layer_idx` "
-#                 "when creating this class."
-#             )
+    def __init__(self, config: MistralConfig, layer_idx: Optional[int] = None):
+        super().__init__()
+        self.config = config
+        self.layer_idx = layer_idx
+        if layer_idx is None:
+            logger.warning_once(
+                f"Instantiating {self.__class__.__name__} without passing a `layer_idx` is not recommended and will "
+                "lead to errors during the forward call if caching is used. Please make sure to provide a `layer_idx` "
+                "when creating this class."
+            )
 
-#         self.attention_dropout = config.attention_dropout
-#         self.hidden_size = config.hidden_size
-#         self.num_heads = config.num_attention_heads
-#         self.head_dim = config.head_dim
-#         self.num_key_value_heads = config.num_key_value_heads
-#         self.num_key_value_groups = self.num_heads // self.num_key_value_heads
-#         self.max_position_embeddings = config.max_position_embeddings
-#         self.rope_theta = config.rope_theta
-#         self.is_causal = False
+        self.attention_dropout = config.attention_dropout
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = config.head_dim
+        self.num_key_value_heads = config.num_key_value_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+        self.max_position_embeddings = config.max_position_embeddings
+        self.rope_theta = config.rope_theta
+        self.is_causal = False
 
-#         self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim)
-#         self.k_proj = nn.Linear(
-#             self.hidden_size,
-#             self.num_key_value_heads * self.head_dim,
-#         )
-#         self.v_proj = nn.Linear(
-#             self.hidden_size,
-#             self.num_key_value_heads * self.head_dim,
-#         )
-#         self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size)
+        self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim)
+        self.k_proj = nn.Linear(
+            self.hidden_size,
+            self.num_key_value_heads * self.head_dim,
+        )
+        self.v_proj = nn.Linear(
+            self.hidden_size,
+            self.num_key_value_heads * self.head_dim,
+        )
+        self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size)
 
-#         self.rotary_emb = MistralRotaryEmbedding(config=self.config)
+        self.rotary_emb = MistralRotaryEmbedding(config=self.config)
 
-#     def forward(
-#         self,
-#         hidden_states: torch.Tensor,
-#         attention_mask: Optional[torch.Tensor] = None,
-#         position_ids: Optional[torch.LongTensor] = None,
-#         past_key_value: Optional[Cache] = None,
-#         output_attentions: bool = False,
-#         use_cache: bool = False,
-#         cache_position: Optional[torch.LongTensor] = None,
-#         position_embeddings: Optional[
-#             Tuple[torch.Tensor, torch.Tensor]
-#         ] = None,  # will become mandatory in v4.46
-#         **kwargs,
-#     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-#         bsz, q_len, _ = hidden_states.size()
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[
+            Tuple[torch.Tensor, torch.Tensor]
+        ] = None,  # will become mandatory in v4.46
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        bsz, q_len, _ = hidden_states.size()
 
-#         if self.config.pretraining_tp > 1:
-#             key_value_slicing = (
-#                 self.num_key_value_heads * self.head_dim
-#             ) // self.config.pretraining_tp
-#             query_slices = self.q_proj.weight.split(
-#                 (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
-#             )
-#             key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
-#             value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
+        if self.config.pretraining_tp > 1:
+            key_value_slicing = (
+                self.num_key_value_heads * self.head_dim
+            ) // self.config.pretraining_tp
+            query_slices = self.q_proj.weight.split(
+                (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
+            )
+            key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
+            value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
 
-#             query_states = [
-#                 F.linear(hidden_states, query_slices[i])
-#                 for i in range(self.config.pretraining_tp)
-#             ]
-#             query_states = torch.cat(query_states, dim=-1)
+            query_states = [
+                F.linear(hidden_states, query_slices[i])
+                for i in range(self.config.pretraining_tp)
+            ]
+            query_states = torch.cat(query_states, dim=-1)
 
-#             key_states = [
-#                 F.linear(hidden_states, key_slices[i])
-#                 for i in range(self.config.pretraining_tp)
-#             ]
-#             key_states = torch.cat(key_states, dim=-1)
+            key_states = [
+                F.linear(hidden_states, key_slices[i])
+                for i in range(self.config.pretraining_tp)
+            ]
+            key_states = torch.cat(key_states, dim=-1)
 
-#             value_states = [
-#                 F.linear(hidden_states, value_slices[i])
-#                 for i in range(self.config.pretraining_tp)
-#             ]
-#             value_states = torch.cat(value_states, dim=-1)
+            value_states = [
+                F.linear(hidden_states, value_slices[i])
+                for i in range(self.config.pretraining_tp)
+            ]
+            value_states = torch.cat(value_states, dim=-1)
 
-#         else:
-#             query_states = self.q_proj(hidden_states)
-#             key_states = self.k_proj(hidden_states)
-#             value_states = self.v_proj(hidden_states)
+        else:
+            query_states = self.q_proj(hidden_states)
+            key_states = self.k_proj(hidden_states)
+            value_states = self.v_proj(hidden_states)
 
-#         query_states = query_states.view(
-#             bsz, q_len, self.num_heads, self.head_dim
-#         ).transpose(1, 2)
-#         key_states = key_states.view(
-#             bsz, q_len, self.num_key_value_heads, self.head_dim
-#         ).transpose(1, 2)
-#         value_states = value_states.view(
-#             bsz, q_len, self.num_key_value_heads, self.head_dim
-#         ).transpose(1, 2)
+        query_states = query_states.view(
+            bsz, q_len, self.num_heads, self.head_dim
+        ).transpose(1, 2)
+        key_states = key_states.view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim
+        ).transpose(1, 2)
+        value_states = value_states.view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim
+        ).transpose(1, 2)
 
-#         if position_embeddings is None:
-#             logger.warning_once(
-#                 "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
-#                 "through `position_ids` (2D tensor with the indexes of the tokens), to using externally computed "
-#                 "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.46 `position_ids` will be "
-#                 "removed and `position_embeddings` will be mandatory."
-#             )
-#             cos, sin = self.rotary_emb(value_states, position_ids)
-#         else:
-#             cos, sin = position_embeddings
-#         query_states, key_states = apply_rotary_pos_emb(
-#             query_states, key_states, cos, sin
-#         )
+        if position_embeddings is None:
+            logger.warning_once(
+                "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
+                "through `position_ids` (2D tensor with the indexes of the tokens), to using externally computed "
+                "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.46 `position_ids` will be "
+                "removed and `position_embeddings` will be mandatory."
+            )
+            cos, sin = self.rotary_emb(value_states, position_ids)
+        else:
+            cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(
+            query_states, key_states, cos, sin
+        )
 
-#         if past_key_value is not None:
-#             # sin and cos are specific to RoPE models; cache_position needed for the static cache
-#             cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
-#             key_states, value_states = past_key_value.update(
-#                 key_states, value_states, self.layer_idx, cache_kwargs
-#             )
+        if past_key_value is not None:
+            # sin and cos are specific to RoPE models; cache_position needed for the static cache
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+            key_states, value_states = past_key_value.update(
+                key_states, value_states, self.layer_idx, cache_kwargs
+            )
 
-#         key_states = repeat_kv(key_states, self.num_key_value_groups)
-#         value_states = repeat_kv(value_states, self.num_key_value_groups)
-#         attn_weights = torch.matmul(
-#             query_states, key_states.transpose(2, 3)
-#         ) / math.sqrt(self.head_dim)
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+        attn_weights = torch.matmul(
+            query_states, key_states.transpose(2, 3)
+        ) / math.sqrt(self.head_dim)
 
-#         if attention_mask is not None:  # no matter the length, we just slice it
-#             causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
-#             attn_weights = attn_weights + causal_mask
+        if attention_mask is not None:  # no matter the length, we just slice it
+            causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+            attn_weights = attn_weights + causal_mask
 
-#         # upcast attention to fp32
-#         attn_weights = nn.functional.softmax(
-#             attn_weights, dim=-1, dtype=torch.float32
-#         ).to(query_states.dtype)
-#         attn_weights = nn.functional.dropout(
-#             attn_weights, p=self.attention_dropout, training=self.training
-#         )
-#         attn_output = torch.matmul(attn_weights, value_states)
+        # upcast attention to fp32
+        attn_weights = nn.functional.softmax(
+            attn_weights, dim=-1, dtype=torch.float32
+        ).to(query_states.dtype)
+        attn_weights = nn.functional.dropout(
+            attn_weights, p=self.attention_dropout, training=self.training
+        )
+        attn_output = torch.matmul(attn_weights, value_states)
 
-#         if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
-#             raise ValueError(
-#                 f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
-#                 f" {attn_output.size()}"
-#             )
+        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+            raise ValueError(
+                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f" {attn_output.size()}"
+            )
 
-#         attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.transpose(1, 2).contiguous()
 
-#         attn_output = attn_output.reshape(bsz, q_len, -1)
+        attn_output = attn_output.reshape(bsz, q_len, -1)
 
-#         if self.config.pretraining_tp > 1:
-#             attn_output = attn_output.split(
-#                 self.hidden_size // self.config.pretraining_tp, dim=2
-#             )
-#             o_proj_slices = self.o_proj.weight.split(
-#                 self.hidden_size // self.config.pretraining_tp, dim=1
-#             )
-#             attn_output = sum(
-#                 [
-#                     F.linear(attn_output[i], o_proj_slices[i])
-#                     for i in range(self.config.pretraining_tp)
-#                 ]
-#             )
-#         else:
-#             attn_output = self.o_proj(attn_output)
+        if self.config.pretraining_tp > 1:
+            attn_output = attn_output.split(
+                self.hidden_size // self.config.pretraining_tp, dim=2
+            )
+            o_proj_slices = self.o_proj.weight.split(
+                self.hidden_size // self.config.pretraining_tp, dim=1
+            )
+            attn_output = sum(
+                [
+                    F.linear(attn_output[i], o_proj_slices[i])
+                    for i in range(self.config.pretraining_tp)
+                ]
+            )
+        else:
+            attn_output = self.o_proj(attn_output)
 
-#         if not output_attentions:
-#             attn_weights = None
+        if not output_attentions:
+            attn_weights = None
 
-#         return attn_output, attn_weights, past_key_value
+        return attn_output, attn_weights, past_key_value
 
 
 class MistralDecoderLayer(nn.Module):
@@ -342,7 +341,7 @@ class MistralDecoderLayer(nn.Module):
         hidden_states = self.input_layernorm(hidden_states)
 
         # Self Attention
-        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+        hidden_states, self_attn_weights = self.self_attn(
             hidden_states=hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
@@ -367,8 +366,8 @@ class MistralDecoderLayer(nn.Module):
         if output_attentions:
             outputs += (self_attn_weights,)
 
-        if use_cache:
-            outputs += (present_key_value,)
+        # if use_cache:
+        #     outputs += (present_key_value,)
 
         return outputs
 
