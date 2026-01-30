d0bbc7f8ce53a47a9c7cc070d7e17e59b4493db0
hieungo1410 <hieungo1410@gmail.com>
2026-01-30 15:33:42 +0700

    update

--- src/custom_models/custom_e5_mistral.py
+++ src/custom_models/custom_e5_mistral.py
@@ -341,7 +341,7 @@ class MistralDecoderLayer(nn.Module):
         hidden_states = self.input_layernorm(hidden_states)
 
         # Self Attention
-        hidden_states, self_attn_weights = self.self_attn(
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
             hidden_states=hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
@@ -366,8 +366,8 @@ class MistralDecoderLayer(nn.Module):
         if output_attentions:
             outputs += (self_attn_weights,)
 
-        # if use_cache:
-        #     outputs += (present_key_value,)
+        if use_cache:
+            outputs += (present_key_value,)
 
         return outputs
 
